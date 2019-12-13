cccb14451263fc52b6c0e138d32f835078fba02e
Aaron Haley <15240680+mechdude@users.noreply.github.com>
2019-12-13 06:38:29 -0600

    feat: add demo data

---
+++ backend/demo.py
@@ -0,0 +1,17 @@
+import datetime
+from dateutil.relativedelta import relativedelta
+
+
+def generate_data():
+
+    today = datetime.date.today()
+    last_month = today + relativedelta(months=-1)
+    next_month = today + relativedelta(months=+1)
+
+    d = [{"_id": f"demo-{day.year}-{day.month:02}", "user_id": "demo", "month": f"{day.year}-{day.month:02}"}
+         for day in [today, last_month, next_month]]
+
+    return {'user_id': 'demo', 'plans': d, 'today': [today.year, today.month, today.day]}
+
+if __name__ == '__main__':
+    print(generate_data())
--- backend/index.py
+++ backend/index.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_pymongo import PyMongo
+import demo
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mealgo"
@@ -7,4 +8,8 @@ mongo = PyMongo(app)
 
 @app.route('/')
 def hello_world():
-    return 'success!' #mongo.db.planner.find({"date": '2019-12-12'}).find_one_or_404()
+    return "success!" #mongo.db.planner.find({"date": '2019-12-12'}).find_one_or_404()
+
+@app.route('/demo')
+def index():
+    return demo.generate_data()
