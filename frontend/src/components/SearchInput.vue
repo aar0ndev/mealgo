fcb643cd8548e641dcd6558bda5bf27b3bd281ae
Aaron Haley <15240680+mechdude@users.noreply.github.com>
2020-01-04 17:50:51 -0600

    feat: search input fixes

--- frontend/src/components/SearchInput.vue
+++ frontend/src/components/SearchInput.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="container">
-    <input class="input" type="text" v-model="value" placeholder="Search" ref="input" @blur="onBlur">
+    <input class="input" type="text" :value="value" @input="e => value = e.target.value"
+        placeholder="Search" ref="input" @blur="onBlur">
     <div class="results">
         <div class="result" v-for="result in results" :key="result.id || result.text" @click="selectResult(result)">
             <slot v-bind:result="result">
@@ -13,63 +14,69 @@
 
 <script>
 export default {
-    props: {
-        values: { type: Array, required: true },
-        focus: { type: Boolean, default: false }
-    },
-    data() {
-        return {
-            value: '',
-            words: [],
-            result: ''
-        }
-    },
-    computed: {
-        wordsDict() {
-            const res = {}
-            this.values.forEach(v => {
-                v && v.text &&
-                v.text.split(' ').forEach(w => {
-                    const word = w.toLowerCase()
-                    res[word] = res[word] || []
-                    res[word].push(v)
-                })
-            })
-            return res
-        },
-        results() {
-            const words = Object.keys(this.wordsDict);
-            return this.value ? words.filter(w => w.startsWith(this.value)).reduce((res,w) => { res.push(...this.wordsDict[w].filter(v => res.indexOf(v)<0)); return res},[]) : []
-        }
-    }, 
-
-    methods: {
-        selectResult(result) {
-            this.$emit("change", {...result})
-            this.result = result
-            this.value = ''
-        },
-        focusInput() {
-            this.$refs.input.focus()
-        },
-        onBlur() {
-            setTimeout(() => !this.result && this.$emit("cancel"), 500)
-        }
-    },
-    mounted() {
-        if (this.focus) this.focusInput()
+  props: {
+    values: { type: Array, required: true },
+    focus: { type: Boolean, default: false }
+  },
+  data () {
+    return {
+      value: '',
+      words: [],
+      result: ''
     }
+  },
+  computed: {
+    sortedValues () {
+      return [...this.values].sort((a, b) => a.text.localeCompare(b.text))
+    },
+    wordsDict () {
+      const res = {}
+      this.values.forEach(v => {
+        v && v.text &&
+                v.text.split(' ').forEach(w => {
+                  const word = w.toLowerCase()
+                  res[word] = res[word] || []
+                  res[word].push(v)
+                })
+      })
+      return res
+    },
+    filteredWords () {
+      const words = Object.keys(this.wordsDict)
+      return this.value && words.filter(w => w.startsWith(this.value.toLowerCase()))
+    },
+    results () {
+      return this.value ? this.filteredWords.reduce((res, w) => { res.push(...this.wordsDict[w].filter(v => res.indexOf(v) < 0)); return res }, []) : this.sortedValues
+    }
+  },
+
+  methods: {
+    selectResult (result) {
+      this.$emit('change', { ...result })
+      this.result = result
+      this.value = ''
+    },
+    focusInput () {
+      this.$refs.input.focus()
+    },
+    onBlur () {
+      setTimeout(() => !this.result && this.$emit('cancel'), 500)
+    }
+  },
+  mounted () {
+    if (this.focus) this.focusInput()
+  }
 }
 </script>
 
 <style scoped>
     .results {
-        max-height: 300px;
         overflow: auto;
         border: 1px solid black;
-        position: absolute;
         width: 100%;
         background: white;
+        top: 0;
+        max-height: 100vh;
     }
 
     .result {
@@ -83,10 +90,16 @@ export default {
     }
 
     .container {
-        position: relative;
+        position: fixed;
+        top: 0;
+        width: 100%;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
     }
 
     .input {
         width: 100%;
+        font-size: 14pt;
     }
 </style>
