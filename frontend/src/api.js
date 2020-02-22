b136961950404b37e04697762591702cab3d44aa
Aaron Haley <15240680+mechdude@users.noreply.github.com>
2020-02-22 12:39:17 -0600

    chore: wrap fetch with abstracted api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,59 +1,71 @@
-window.state = {};
+var state = {}
+window.state = state
+
+// provide implementation independent abstraction over api requests
+async function request (url, body = null, method = 'GET', headers = {}, wait = true) {
+  if (wait) {
+    state.$global.waiting = true
+  }
+  try {
+    var res = await fetch(
+      url, {
+        headers: {
+          'Authorization': localStorage.getItem('api-token'),
+          'Accept': 'application/json',
+          'Content-Type': 'application/json;charset=UTF-8',
+          ...headers
+        },
+        body: JSON.stringify(body),
+        method
+      })
+
+    if (res.ok) {
+      return res.json()
+    } else {
+      var err = new Error(`${res.statusText} (${res.status})`)
+      err.res = res
+      throw err
+    }
+  } finally {
+    if (wait) {
+      state.$global.waiting = false
+    }
+  }
+}
 
 export default {
-    async login(email, pass) {
-        var body = {
-            email,
-            password: pass
-        }
-        state.$global.waiting = true;
-        try {
-            var res = await fetch(
-                '/api/login', {
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json;charset=UTF-8'
-                },
-                body: JSON.stringify(body),
-                method: 'POST'
-            })
-
-            if (res.ok) {
-                var token = await res.text()
-                localStorage.setItem('api-token', token)
-                state.$global.loggedIn = true
-            } else {
-                throw new Error(`Unable to login:  ${res.statusText} (${res.status})`)
-            }
-        } finally {
-            state.$global.waiting = false;
-        }
-    },
-    logout() {
-        localStorage.removeItem('api-token')
-        state.$global.loggedIn = false
-    },
-    async get(type, id) {
-        var res = await fetch(
-            '/api/' + type, {
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json;charset=UTF-8',
-                'Authorization': localStorage.getItem('api-token')
-            },
-        })
-        if (res.ok) {
-            return res.json()
-        } else {
-            // todo: handle auth errors
-            throw new Error(`${res.status} (${res.statusText})`)
-        }
-    },
-    setup(vm) {
-        state = vm;
-        var cachedToken = localStorage.getItem('api-token')
-        if (cachedToken) {
-            state.$global.loggedIn = true;
-        }
+  async login (email, pass) {
+    var body = {
+      email,
+      password: pass
     }
+
+    try {
+      var { token } = await request('/api/login', body, 'POST')
+      localStorage.setItem('api-token', token)
+      state.$global.loggedIn = true
+    } catch (err) {
+      throw new Error(`Unable to login:  ${err.res.statusText} (${err.res.status})`)
+    }
+  },
+  logout () {
+    localStorage.removeItem('api-token')
+    state.$global.loggedIn = false
+  },
+  async get (type, id = null) {
+    try {
+      var res = await request(`/api/${type}` + id ? `/${id}` : '')
+      return res.json()
+    } catch (err) {
+      // todo: handle auth errors
+      throw new Error(`${res.status} (${res.statusText})`)
+    }
+  },
+  setup (vm) {
+    state = vm
+    var cachedToken = localStorage.getItem('api-token')
+    if (cachedToken) {
+      state.$global.loggedIn = true
+    }
+  }
 }
